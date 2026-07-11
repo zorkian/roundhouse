4714b848ff86d1e9534eb68edbeb6748759419ce
Mark Smith <mark@qq.is>
2026-07-11 15:41:23 -0700

    Add bounded Codex implementation adapter

--- packages/self-development/src/index.ts
+++ packages/self-development/src/index.ts
@@ -1,6 +1,7 @@
 // Copyright 2026 Mark Smith
 // SPDX-License-Identifier: Apache-2.0
 
+export * from "./codex-adapter.js";
 export * from "./run-store.js";
 export * from "./task.js";
 export * from "./workspace.js";
